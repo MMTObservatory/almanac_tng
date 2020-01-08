02a92cde96d61da6ab1f7c17b020cb07cc9e0142
T. E. Pickering <te.pickering@gmail.com>
2020-01-08 13:59:46 -0700

    add test for ascii printing

---
+++ mmt_almanac/tests/test_ascii.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# coding=utf-8
+
+from ..almanac import nightly_almanac, ascii_night
+
+def test_ascii_night():
+    a = nightly_almanac(time="2019-01-01")
+    s = ascii_night(almanac=a)
+    assert(len(s) > 1)
