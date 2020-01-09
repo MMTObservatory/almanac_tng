fd3b0ef02af5a21c2f97b2649d81011deea4217a
T. E. Pickering <te.pickering@gmail.com>
2020-01-08 17:20:12 -0700

    define HM_STR and use it where it applies

--- mmt_almanac/almanac.py
+++ mmt_almanac/almanac.py
@@ -27,6 +27,7 @@ HORIZONS = {
     "18 Deg": -18 * u.deg
 }
 
+HM_STR = "%-H %M"
 MMT_LOCATION = EarthLocation.from_geodetic("-110:53:04.4", "31:41:19.6", 2600 * u.m)
 MMT = astroplan.Observer(name="MMTO", location=MMT_LOCATION, timezone="US/Arizona", pressure=0*u.mbar)
 
@@ -42,14 +43,6 @@ def nearest_minute(dt):
     USNO predictions round to nearest minute so we use this hack to follow that.
     """
     return (dt + datetime.timedelta(seconds=30)).replace(second=0, microsecond=0)
-
-
-def hm_string(dt):
-    """
-    The traditional MMTO almanac format displays time as "HH MM" with no leading zero on the hour.
-    """
-    hm_str = nearest_minute(dt).strftime("%-H %M")
-    return hm_str
 
 
 def calc_newmoons(time=Time.now(), nmonths=2):
@@ -119,11 +112,11 @@ def ascii_night(almanac=nightly_almanac()):
     Takes a dict() as produced by nightly_almanac() and prints out string in a format that matches the MMTO's
     printed almanac.
     """
-    tset_str = nearest_minute(almanac['Sunset'].to_datetime(timezone=TZ)).strftime("%-H %M")
+    tset_str = nearest_minute(almanac['Sunset'].to_datetime(timezone=TZ)).strftime(HM_STR)
 
     eve_str = ""
     for h in HORIZONS.keys():
-        eve_str += nearest_minute(almanac[f"Eve {h}"].to_datetime(timezone=TZ)).strftime("%-H %M")
+        eve_str += nearest_minute(almanac[f"Eve {h}"].to_datetime(timezone=TZ)).strftime(HM_STR)
         eve_str += "   "
 
     ra3_west_str = "{:02d} {:02d}".format(int(almanac['RA 3 Hr West'].hms.h), int(almanac['RA 3 Hr West'].hms.m))
@@ -136,20 +129,20 @@ def ascii_night(almanac=nightly_almanac()):
     keys = list(HORIZONS.keys())
     keys.reverse()
     for h in keys:
-        morn_str += nearest_minute(almanac[f"Morn {h}"].to_datetime(timezone=TZ)).strftime("%-H %M")
+        morn_str += nearest_minute(almanac[f"Morn {h}"].to_datetime(timezone=TZ)).strftime(HM_STR)
         morn_str += "   "
 
-    trise_str = nearest_minute(almanac['Sunrise'].to_datetime(timezone=TZ)).strftime("%-H %M").format("{:5s}")
+    trise_str = nearest_minute(almanac['Sunrise'].to_datetime(timezone=TZ)).strftime(HM_STR).format("{:5s}")
 
     moon_rise = almanac['Moon Rise']
     moon_set = almanac['Moon Set']
 
     if moon_rise < almanac['Sunrise'] and moon_rise > almanac['Sunset']:
-        mr_str = nearest_minute(moon_rise.to_datetime(timezone=TZ)).strftime("%-H %M")
+        mr_str = nearest_minute(moon_rise.to_datetime(timezone=TZ)).strftime(HM_STR)
     else:
         mr_str = "     "
     if moon_set > almanac['Sunset'] and moon_set < almanac['Sunrise']:
-        ms_str = nearest_minute(moon_set.to_datetime(timezone=TZ)).strftime("%-H %M")
+        ms_str = nearest_minute(moon_set.to_datetime(timezone=TZ)).strftime(HM_STR)
     else:
         ms_str = "     "
 
