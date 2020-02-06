65620934ef165409c16c315828d69898c99344d9
T. E. Pickering <te.pickering@gmail.com>
2020-02-06 16:30:21 -0700

    more structural updates to match APE 17, new astropy template, and what's been done in mmtwfs

--- mmt_almanac/__init__.py
+++ mmt_almanac/__init__.py
@@ -1,8 +1,25 @@
-# Set up the version
-from pkg_resources import get_distribution, DistributionNotFound
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = 'unknown'
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *   # noqa
+# ----------------------------------------------------------------------------
+
+# Enforce Python version check during package import.
+# This is the same check as the one at the top of setup.py
+import sys
+from distutils.version import LooseVersion
+
+__minimum_python_version__ = "3.7"
+
+__all__ = []
+
+
+class UnsupportedPythonError(Exception):
+    pass
+
+
+if LooseVersion(sys.version) < LooseVersion(__minimum_python_version__):
+    raise UnsupportedPythonError("mmtwfs does not support Python < {}"
+                                 .format(__minimum_python_version__))
