9dc78e3a6065759f2c3ac7894b03b39f05025ae9
T. E. Pickering <te.pickering@gmail.com>
2020-12-30 17:26:19 -0700

    add tests to cover the new ascii methods

--- mmt_almanac/tests/test_ascii.py
+++ mmt_almanac/tests/test_ascii.py
@@ -1,9 +1,29 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # coding=utf-8
+import argparse
+from unittest import mock
 
-from ..ascii import ascii_night
+from ..ascii import ascii_night, ascii_month, ascii_year, yearly_almanac
 
 
 def test_ascii_night():
     s = ascii_night()
+    assert(len(s) > 1)
+
+
+def test_ascii_month():
+    s = ascii_month()
+    assert(len(s) > 1)
+
+
+def test_ascii_year():
+    s = ascii_year()
+    assert(len(s) > 1)
+
+
+@mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(outfile='almanac.txt', year=2021))
+def test_yearly_almanac(mock_args):
+    yearly_almanac()
+    with open("almanac.txt", 'r') as fp:
+        s = fp.read()
     assert(len(s) > 1)
